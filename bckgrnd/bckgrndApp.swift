f4999efc3d41270cdd7842d9eeaff5207e0ac216
itsmeabhi12 <abhishek.ghimire281@gmail.com>
2024-05-08 15:37:17 +0545

    Initial Commit

---
+++ bckgrnd/bckgrndApp.swift
@@ -0,0 +1,17 @@
+//
+//  bckgrndApp.swift
+//  bckgrnd
+//
+//  Created by ebpearls on 8/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct bckgrndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
