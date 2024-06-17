b0482b5bacc5141ab4f6d9dcaa7e443f8fc7b492
itsmeabhi12 <abhishek.ghimire281@gmail.com>
2024-06-17 12:22:12 +0545

    Background Task

--- bckgrnd/ContentView.swift
+++ bckgrnd/ContentView.swift
@@ -6,14 +6,34 @@
 //
 
 import SwiftUI
+import BackgroundTasks
 
 struct ContentView: View {
+    
+    @State var  currentPendingTasks : String = ""
+    
+    
+    var executionNumer : String {
+        let data =   UserDefaults.standard.integer(forKey: BGProcessingTaskIdentifier)
+        return "\(data)"
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Pending Tasks : \(currentPendingTasks)")
+                .padding(8)
+            Text("Background iteration")
+            Text(executionNumer)
+            Button("schedule task") {
+                scheduleBGProcessingTask()
+            }.padding(8)
+            Button("cancel task") {
+                cancealalltasks();
+            }.padding(8)
+            Button("print sceduled task") {
+                printPendingTask()
+            }.padding(8)
+            
         }
         .padding()
     }
@@ -21,4 +41,47 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+extension ContentView {
+    func scheduleBGAppRefreshTask() {
+        BGTaskScheduler.shared.getPendingTaskRequests(completionHandler: { request in
+            if request.isEmpty {
+                let request = BGAppRefreshTaskRequest(identifier: BGAppRefreshTaskIdentifier)
+                request.earliestBeginDate = Date(timeIntervalSinceNow: 15 * 60)
+                do{
+                    try BGTaskScheduler.shared.submit(request)
+                    print("Scheduled")
+                }catch{
+                    print(error)
+                }
+            }
+        })
+    }
+    
+    func scheduleBGProcessingTask() {
+        BGTaskScheduler.shared.getPendingTaskRequests(completionHandler: { request in
+            if request.isEmpty {
+                let request = BGProcessingTaskRequest(identifier: BGProcessingTaskIdentifier)
+                request.earliestBeginDate = Date(timeIntervalSinceNow: 15 * 60)
+                do{
+                    try BGTaskScheduler.shared.submit(request)
+                    print("Scheduled")
+                    
+                }catch{
+                    print(error)
+                }
+            }
+        })
+    }
+    
+    func printPendingTask() {
+        BGTaskScheduler.shared.getPendingTaskRequests(completionHandler: { request in
+            currentPendingTasks  = "\(request)"
+        })
+    }
+    
+    func cancealalltasks() {
+        BGTaskScheduler.shared.cancelAllTaskRequests();
+    }
 }
--- bckgrnd/bckgrndApp.swift
+++ bckgrnd/bckgrndApp.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
+import BackgroundTasks
+
+let BGAppRefreshTaskIdentifier : String = "BGAppRefreshTaskIdentifier"
+let BGProcessingTaskIdentifier : String = "BGProcessingTaskIdentifier"
 
 @main
 struct bckgrndApp: App {
+    
+    
+    func handleBGAppRefreshTask() {
+        let data =  UserDefaults.standard.integer(forKey: BGAppRefreshTaskIdentifier)
+        UserDefaults.standard.set(data + 1, forKey: BGAppRefreshTaskIdentifier)
+        print("BGAppRefreshTask Executed<-----");
+    }
+    
+    func handleBGProcessingTask() {
+        let data =  UserDefaults.standard.integer(forKey: BGProcessingTaskIdentifier)
+        UserDefaults.standard.set(data + 1, forKey: BGProcessingTaskIdentifier)
+       
+    }
+    
+
+    
+    func canacelAllTask() {
+        BGTaskScheduler.shared.cancelAllTaskRequests();
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+           ContentView()
         }
+        .backgroundTask(.appRefresh(BGAppRefreshTaskIdentifier)) { _ in
+            handleBGAppRefreshTask()
+        }
+        .backgroundTask(.appRefresh(BGProcessingTaskIdentifier), action: { _ in
+            handleBGProcessingTask()
+        })
     }
 }
+
